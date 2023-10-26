769725ef97d4c596913da79f2b1b07b181542d9e
eight-bitguy <sandalj6@gmail.com>
2023-10-26 22:32:36 +0530

    adding volme in docker

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ window.getRoomCode = () => {
     return localStorage.getItem('room-code');
 };
 
-window.apiUrl = () => 'http://dehla-pakad-api.eightbitguy.in/8000';
+window.apiUrl = () => 'https://dehla-pakad-api.eightbitguy.in';
 
 addInterceptor();
 ReactDOM.render(
